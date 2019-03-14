b81e323eac39bfff719f3cdc3d74836a1c45ba73
gogumassafy <gogumassafy@gmail.com>
2019-03-14 06:12:38 +0000

    01_1:N model relation

--- boards/urls.py
+++ boards/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-    path('<int:pk>/edit/', views.edit, name='edit'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
-    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:board_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('<int:board_pk>/comments/', views.comments_create, name='comments_create'),
+    path('<int:board_pk>/edit/', views.edit, name='edit'),
+    path('<int:board_pk>/delete/', views.delete, name='delete'),
+    path('<int:board_pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
     path('', views.index, name='index'),
 ]
